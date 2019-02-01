11c68bed9453529a5464e8df1ad6be49ec47e6d9
Luiz Pereira <luizfper@gmail.com>
2019-02-01 21:37:03 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  val =99999
 
+  name_hash.each do |key,value|
+    if val >value
+      val=value
+      chave=key
+    end
+  end
+  chave
 end
